1003db4ae12d39845ddff1358c857d655a987921
Mariusz <mariusz.staniak@gmail.com>
2024-08-12 21:07:04 +0200

    price calculator tests

--- src/main/java/org/example/pricecalculator/domain/discount/PriceCalculator.java
+++ src/main/java/org/example/pricecalculator/domain/discount/PriceCalculator.java
@@ -3,9 +3,19 @@ package org.example.pricecalculator.domain.discount;
 import org.example.pricecalculator.domain.product.Amount;
 import org.example.pricecalculator.domain.product.Price;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class PriceCalculator {
 
+    /**
+     * Lets make assumption that lowest possible price is 0.01 $
+     */
     public Price calculate(Price unitPrice, Amount amount) {
-        return null;
+        var total = unitPrice.price().multiply(amount.amount()).setScale(2, RoundingMode.HALF_UP);
+        if (total.equals(new BigDecimal("0.00"))) {
+            total = new BigDecimal("0.01");
+        }
+        return new Price(total, unitPrice.currency());
     }
 }
--- src/test/java/org/example/pricecalculator/domain/discount/PriceCalculatorTest.java
+++ src/test/java/org/example/pricecalculator/domain/discount/PriceCalculatorTest.java
@@ -4,7 +4,8 @@ import org.example.pricecalculator.domain.product.Amount;
 import org.example.pricecalculator.domain.product.Currency;
 import org.example.pricecalculator.domain.product.Price;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
@@ -12,13 +13,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PriceCalculatorTest {
 
-    @Test
-    void shouldReturnTotalPrice() {
+    @ParameterizedTest
+    @CsvSource({
+            "1.00,1.00,1.00",
+            "2.00,4.00,8.00",
+            "0.01,0.01,0.01", //assumption that lowest possible price is 0.01$
+            "10.01,1.01,10.11",
+            "10.01,1.01,10.11",
+            "10.10,0.33,3.33",
+    })
+    void shouldReturnTotalPrice(String givenPriceString, String givenAmountString, String expectedTotalPriceString) {
         // given
         var priceCalculator = new PriceCalculator();
-        var givenPrice = new Price(new BigDecimal("1.00"), Currency.of("PLN"));
-        var givenAmount = Amount.of(new BigDecimal("1.00"));
-        var expectedTotalPrice = new Price(new BigDecimal("1.00"), Currency.of("PLN"));
+        var givenPrice = new Price(new BigDecimal(givenPriceString), Currency.of("PLN"));
+        var givenAmount = Amount.of(new BigDecimal(givenAmountString));
+        var expectedTotalPrice = new Price(new BigDecimal(expectedTotalPriceString), Currency.of("PLN"));
         // when
         var result = priceCalculator.calculate(givenPrice, givenAmount);
 
