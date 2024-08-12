e919c8f1627b11c9f7866a898b7c52fc7e0227a3
Mariusz <mariusz.staniak@gmail.com>
2024-08-12 21:13:30 +0200

    price tests

--- src/main/java/org/example/pricecalculator/domain/product/Price.java
+++ src/main/java/org/example/pricecalculator/domain/product/Price.java
@@ -3,4 +3,15 @@ package org.example.pricecalculator.domain.product;
 import java.math.BigDecimal;
 
 public record Price(BigDecimal price, Currency currency) {
+    public Price {
+        if (price == null) {
+            throw new IllegalArgumentException("Price must be provided");
+        }
+        if (price.compareTo(BigDecimal.ZERO) == 0) {
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+        if (currency == null) {
+            throw new IllegalArgumentException("Currency must be provided");
+        }
+    }
 }
---
+++ src/test/java/org/example/pricecalculator/domain/discount/PriceTest.java
@@ -0,0 +1,44 @@
+package org.example.pricecalculator.domain.discount;
+
+import org.example.pricecalculator.domain.product.Currency;
+import org.example.pricecalculator.domain.product.Price;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PriceTest {
+
+    @Test
+    void shouldThrowExceptionWhenPriceIsNull() {
+        // expected
+        var thrown = assertThrows(IllegalArgumentException.class, () -> new Price(null, Currency.of("PLN")));
+
+        assertEquals("Price must be provided", thrown.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPriceIsZero() {
+        // expected
+        var thrown = assertThrows(IllegalArgumentException.class, () -> new Price(new BigDecimal("0.00"), Currency.of("PLN")));
+
+        assertEquals("Price must be greater than zero", thrown.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCurrencyIsNotProvided() {
+        // expected
+        var thrown = assertThrows(IllegalArgumentException.class, () -> new Price(new BigDecimal("0.01"), null));
+
+        assertEquals("Currency must be provided", thrown.getMessage());
+    }
+
+    @Test
+    void shouldBuildPriceObject() {
+        // expected
+        var price = new Price(new BigDecimal("0.01"), new Currency("PLN"));
+
+        assertNotNull(price);
+    }
+}
