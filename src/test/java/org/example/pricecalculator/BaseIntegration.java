a9231a5977767b6552102773fddde51b2ae9461e
Mariusz <mariusz.staniak@gmail.com>
2024-08-13 13:28:19 +0200

    move tests to dedicated packages

---
+++ src/test/java/org/example/pricecalculator/BaseIntegration.java
@@ -0,0 +1,10 @@
+package org.example.pricecalculator;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+public class BaseIntegration {
+}
